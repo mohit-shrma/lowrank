1fdbd09aae0a52742cb90d6d7dbc26a47a8d11ac
Mohit Sharma <mohit.786.sharma@gmail.com>
2015-11-16 23:18:07 -0600

    updated er1mp code

--- ER1MP.py
+++ ER1MP.py
@@ -8,16 +8,40 @@ def ER1MP(Y_sp, rank):
   nrows, ncols = obsSpMat.shape
   X = np.zeros((nrows, ncols))
   
+  #TODO
+  (row_inds, col_inds) = Y_sp.nnzind
+  
+  #TODO: impleament proj operator
+  Y_proj
+  b = Y_sp[row_inds, col_inds]
+  theta = np.zeros(rank)
+  us = []
+  vs = []
   for k in range(rank):
     #find top singular vector of Y_sp - X
-    (u, v) = topSVD(Y_sp - X)
+    u, s, v = sp.linalg.svds(Y_proj - X, 1)
+    us.append(u)
+    vs.append(v)
     M_k = np.outer(u,v)
     
     #solve least square to find optimal alphas, \alpha_k
     #TODO:
-    np.linalg.lstsq(A,b)
+    a1 = X.reshape(nrows*ncols, 1)
+    a2 = M_k_proj.reshape(nrows*ncols, 1)
+    b = Y_proj.reshape(nrows*ncols, 1)
+    A = np.vstack((a1, a2))
+    alpha1, alpha2 = np.linalg.lstsq(A.T, b)
+    
+    X = alpha1*X + alpha2*M_k_proj
+    theta[k] = alpha2
+    for i in range(k-1):
+      theta[i] = theta[i]*alpha1
 
-
+  #return output matrix
+  opMat = np.zeros(nrows, ncols)
+  for k in range(rank):
+    opMat += theta[k]*np.outer(us[k],vs[k])
+  return opMat
 
 
 def main():
