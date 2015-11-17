5e9899288a9023c10dfcd35189da2583bcd501ea
mohit sharma <sharm163@umn.edu>
2015-11-17 16:01:32 -0600

    added fixed er1mp routine

--- ER1MP.py
+++ ER1MP.py
@@ -1,46 +1,59 @@
 import numpy as np 
 import scipy as sp
+import scipy.sparse.linalg as splinalg
 import sys
 from sparsesvd import sparsesvd
 
-def ER1MP(Y_sp, rank):
 
-  nrows, ncols = obsSpMat.shape
+def getProjMat(mat, rowInds, colInds):
+  nrows, ncols = mat.shape 
+  mMat = np.zeros((nrows, ncols))
+  mMat[rowInds, colInds] = 1 
+  projMat = np.multiply(mat, mMat)
+  return projMat
+
+
+def ER1MP(Y, rank):
+
+  nrows, ncols = Y.shape
   X = np.zeros((nrows, ncols))
+  row_inds, col_inds = np.nonzero(Y)
   
-  #TODO
-  (row_inds, col_inds) = Y_sp.nnzind
-  
-  #TODO: impleament proj operator
-  Y_proj
-  b = Y_sp[row_inds, col_inds]
+  #get non-zero indices projection of Y
+  Y_proj = getProjMat(Y, row_inds, col_inds)
+  b = Y_proj.reshape(nrows*ncols, 1)
   theta = np.zeros(rank)
   us = []
   vs = []
+
   for k in range(rank):
+    
     #find top singular vector of Y_sp - X
-    u, s, v = sp.linalg.svds(Y_proj - X, 1)
+    u, s, v = splinalg.svds(Y_proj - X, 1)
     us.append(u)
     vs.append(v)
     M_k = np.outer(u,v)
+    M_k_proj = getProjMat(M_k, row_inds, col_inds) 
     
     #solve least square to find optimal alphas, \alpha_k
-    #TODO:
     a1 = X.reshape(nrows*ncols, 1)
     a2 = M_k_proj.reshape(nrows*ncols, 1)
-    b = Y_proj.reshape(nrows*ncols, 1)
-    A = np.vstack((a1, a2))
-    alpha1, alpha2 = np.linalg.lstsq(A.T, b)
-    
+    A = np.hstack((a1, a2))
+    sol = np.linalg.lstsq(A, b)
+    alphas = sol[0]
+    alpha1 = alphas[0]
+    alpha2 = alphas[1]
+
     X = alpha1*X + alpha2*M_k_proj
     theta[k] = alpha2
     for i in range(k-1):
       theta[i] = theta[i]*alpha1
 
   #return output matrix
-  opMat = np.zeros(nrows, ncols)
+  opMat = np.zeros((nrows, ncols))
   for k in range(rank):
     opMat += theta[k]*np.outer(us[k],vs[k])
+
   return opMat
 
 
@@ -50,9 +63,6 @@ def main():
   obsMatName = sys.argv[1]
   rank = int(sys.argv[2])
   
-
-
-
 
 if __name__ == '__main__':
   main()
